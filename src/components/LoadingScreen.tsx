943f166ed86910c65f490081f732b9248923e2c2
brendanmcoyne <bmcoyne@bu.edu>
2025-07-14 20:43:43 -0400

    Loading screen trial 1

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,21 +1,13 @@
 import styled, { keyframes } from "styled-components";
 
 const grow = keyframes`
-    from {
-        transform: scale(1);
-    }
-    to {
-        transform: scale(1.4);
-    }
+    from { transform: scale(1); }
+    to { transform: scale(1.4); }
 `;
 
 const spin = keyframes`
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(720deg);
-    }
+    from { transform: rotate(0deg); }
+    to { transform: rotate(720deg); }
 `;
 
 const Overlay = styled.div`
@@ -23,64 +15,77 @@ const Overlay = styled.div`
     inset: 0;
     z-index: 9999;
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start; 
+    padding-top: 10rem; 
+    background-color: darkblue;
+    overflow: hidden;
+
+    @media screen and (max-width: 1000px) {
+        padding-top: 5rem;
+    }
+`;
+
+const LogoWrapper = styled.div`
+    position: relative;
+    width: 25rem;
+    height: 25rem;
+    display: flex;
     align-items: center;
     justify-content: center;
-    background-color: darkblue;
-    flex-direction: column;
-    overflow: hidden;
+    z-index: 1;
+
+    @media screen and (max-width: 1000px) {
+        width: 17rem;
+        height: 17rem;
+    }
 `;
 
 const SpinningBackground = styled.img`
     position: absolute;
-    top: 10%;
     width: 25rem;
     height: 25rem;
-    margin-bottom: 20px;
-    opacity: .8;
+    opacity: 0.8;
     animation: ${spin} 5s linear infinite;
     object-fit: contain;
     z-index: 0;
+
     @media screen and (max-width: 1000px) {
-        margin-top: 6.5rem;
-        width: 15rem;
-        height: 15rem;
+        width: 17rem;
+        height: 17rem;
     }
 `;
 
 const Image = styled.img`
-    width: 20rem;
-    height: 20rem;
-    margin-top: 250px;
+    width: 80%;
+    height: 80%;
     object-fit: contain;
-    text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.8);
     animation: ${grow} 1s ease-out forwards;
     z-index: 1;
-
-    @media screen and (max-width: 1000px) {
-        width: 12rem;
-        height: 12rem;
-        margin-top: 180px;
-    }
 `;
 
 const Header = styled.h2`
     font-size: 150px;
     color: white;
     z-index: 2;
-    margin-bottom: 100px;
+    margin-top: 80px;
     font-family: 'Luckiest Guy', cursive;
     text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000, 0px 0px 12px #ff0;
+
     @media screen and (max-width: 1000px) {
         font-size: 75px;
-        margin-bottom: 100px;
+        margin-top: 80px;
     }
 `;
 
 export default function LoadingScreen({ message = "Loading" }) {
     return (
         <Overlay>
-            <SpinningBackground src="/baseball.webp" alt="Background" />
-            <Image src="/LeagueLogo_fix.png" alt="Loading..." />
+            <LogoWrapper>
+                <SpinningBackground src="/baseball.webp" alt="Background" />
+                <Image src="/LeagueLogo_fix.png" alt="Loading..." />
+            </LogoWrapper>
             <Header>{message}</Header>
         </Overlay>
     );
